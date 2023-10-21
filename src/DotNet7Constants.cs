53301860f2aa6ec0f0b9e6e1baa90d672d36ae24
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-21 14:30:04 +0200

    Updated to .NET v7.0.12

--- src/DotNet7Constants.cs
+++ src/DotNet7Constants.cs
@@ -9,19 +9,19 @@ public static class DotNet7Constants
     ///     .NET 7 Desktop Runtime direct download link (x64).
     /// </summary>
     public const string DotNet7DesktopX64Url =
-        "https://download.visualstudio.microsoft.com/download/pr/7727acb3-25ca-473b-a392-75afeb33cab7/f11f0477fd2fcfbb3111881377d0c9bb/windowsdesktop-runtime-7.0.9-win-x64.exe";
+        "https://download.visualstudio.microsoft.com/download/pr/f9ea536d-8e1f-4247-88b8-e79e33fa0873/c06e39f73a3bb1ec8833bb1cde98fce3/windowsdesktop-runtime-7.0.12-win-x64.exe";
 
     /// <summary>
     ///     .NET 7 Desktop Runtime direct download link (x86).
     /// </summary>
     public const string DotNet7DesktopX86Url =
-        "https://download.visualstudio.microsoft.com/download/pr/139b19d0-2d39-48ce-b59a-aec437509c20/ea6a2711eec53660c3b14d78b9fb2963/windowsdesktop-runtime-7.0.9-win-x86.exe";
+        "https://download.visualstudio.microsoft.com/download/pr/7e606599-560e-47c9-9ff1-60af4beaffb5/377fdff249559470cf483880f16acaf0/windowsdesktop-runtime-7.0.12-win-x86.exe";
 
     /// <summary>
     ///     ASP.NET Core Hosting Bundle direct download link.
     /// </summary>
     public const string AspNetCoreHostingBundleUrl =
-        "https://download.visualstudio.microsoft.com/download/pr/a1918362-b09b-4593-a4b1-e5f0d9bd68b0/2470e7376871b57867655c057e878800/dotnet-hosting-7.0.9-win.exe";
+        "https://download.visualstudio.microsoft.com/download/pr/723dac98-2f86-4efa-a67d-90b7f88d1a78/325c7069d70a1b23dd1a1545930ac251/dotnet-hosting-7.0.12-win.exe";
 
     /// <summary>
     ///     Desktop Runtime name to check against when enumerating runtimes.
